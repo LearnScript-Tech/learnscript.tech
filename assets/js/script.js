102e73a369b66b00605bd5d19320a9f7d303375c
Dev Sharma <dev.18715@knit.ac.in>
2020-12-22 19:10:36 +0530

    Updated

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+//Header Fixed
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  } else {
+    $('#header').removeClass('header-scrolled');
+  }
+});
+
+if ($(window).scrollTop() > 100) {
+  $('#header').addClass('header-scrolled');
+}
+
+
+//Initialize VenoBox
+
+$(window).on('load', function() {
+  $('.venobox').venobox({
+    bgcolor: '',
+    overlayColor: 'rgba(6, 12, 34, 0.85)',
+    closeBackground: '',
+    closeColor: '#fff',
+    share: false
+  });
+});
+
+//Init AOS
+
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    once: true
+  });
+}
+$(window).on('load', function() {
+  aos_init();
+});
+})(jQuery);
+
+//Back to Top
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+$('.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1500, 'easeInOutExpo');
+  return false;
+});
+
+
+//Activate smooth scroll on page load with hash links in the URl
+
+$(document).ready(function() {
+  if (window.location.hash) {
+    var initial_nav = window.location.hash;
+    if ($(initial_nav).length) {
+      var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+      $('html, body').animate({
+        scrollTop: scrollto
+      }, 1500, 'easeInOutExpo');
+    }
+  }
+});
+
+
+window.addEventListener("scroll",function(){
+  var header= document.querySelector("header");
+  header.classList.toggle("sticky".window.scrollY>0);
+})
+
+
+
+  
